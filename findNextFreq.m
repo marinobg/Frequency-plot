c34eb6e93ae170c53242fbc0135fdc187e2d8952
Marino <mabrgr@gmail.com>
2015-06-24 13:50:47 +0200

    Made the algorithm choose frequency based on distance and surronding frequencies

--- findNextFreq.m
+++ findNextFreq.m
@@ -1,11 +1,14 @@
-function frequency = findNextFreq(d, fbest, indexes, link)
-maxdist = 0;
+function frequency = findNextFreq(d, fbest, indexes, link, nFreq)
+frqs = 1:nFreq;
 %Finds the longest distance to a link that has been assigned with a
 %frequency for the next link that are going to get a frequency assigned
-for i = indexes
-    if d(i, link) > maxdist
-        maxdist = d(i, link);
-        frequency = fbest(i);
-    end
+relevantd = d(indexes, link);
+while length(find(frqs)) > 1
+    [mindist, mindex] = min(relevantd);
+    [row, ~] = find(d==mindist); %Finds link that are closest (and not checked)
+    row(find(row==link)) = []; %Removing the value of the link we want to find a frequency for
+    frqs(fbest(row)) = 0; % Removing frequencies that are closest
+    relevantd(mindex) = []; %Removing shortest distances
 end
+frequency = find(frqs);
 end
