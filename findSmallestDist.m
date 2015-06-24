68ef0617ce674dc5439be4c167f756463d648a03
Marino <mabrgr@gmail.com>
2015-06-24 12:07:11 +0200

    Minor changes (changed name of variable and added a comment)

--- findSmallestDist.m
+++ findSmallestDist.m
@@ -1,10 +1,12 @@
-function next_node = findSmallestDist(d, indexes)
+function next_link = findSmallestDist(d, indexes)
 minval = inf;
+%Finds the link with shortest distance longest distance to other links with
+%assigned frequency
 for i = indexes
     for j = 1:length(d)
         if i ~= j && d(i,j) < minval && ~ismember(j, indexes)
             minval = d(i,j);
-            next_node = j;
+            next_link = j;
         end
     end
 end
