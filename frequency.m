31fa0cc4db4c978e27c253c8e3db2bb1663c5246
Marino <mabrgr@gmail.com>
2015-06-24 10:27:19 +0200

    Added method to choose the first to channels in a wifi network

--- frequency.m
+++ frequency.m
@@ -1,6 +1,6 @@
 function fbest = frequency(d, nFreq)
-fbest = randi(nFreq, 1, length(d));
-dmin = ones(length(d))*inf;
+fbest = nan(1, length(d));
+dmin = ones(1 ,length(d))*inf;
 for i = 1:length(d)
     for j = 1:length(d)
         if i ~= j && d(i,j) < dmin(i)
@@ -8,4 +8,12 @@ for i = 1:length(d)
         end
     end
 end
+[minval, index] = min(dmin);
+fbest(index) = 1;
+for i = index:length(dmin)
+    if dmin(i) == dmin(index)
+        index = i;
+    end
+end
+fbest(index) = 2;
 end
