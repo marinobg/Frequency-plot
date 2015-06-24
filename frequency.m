7896371461bddfbaec606aea4886b9f922b9a41e
Marino <mabrgr@gmail.com>
2015-06-24 11:30:15 +0200

    The for-loop will iterate as long as there are links that does not have a frequency allocated. assignedFreq is a value that will check if all available frequencies are used

--- frequency.m
+++ frequency.m
@@ -1,5 +1,5 @@
 function fbest = frequency(d, nFreq)
-fbest = nan(1, length(d));
+fbest = zeros(1, length(d));
 dmin = ones(1 ,length(d))*inf;
 for i = 1:length(d)
     for j = 1:length(d)
@@ -8,7 +8,9 @@ for i = 1:length(d)
         end
     end
 end
-[minval, index] = min(dmin);
+
+%Finding the closest links and assign them with different channels
+[~, index] = min(dmin);
 fbest(index) = 1;
 for i = index:length(dmin)
     if dmin(i) == dmin(index)
@@ -16,4 +18,13 @@ for i = index:length(dmin)
     end
 end
 fbest(index) = 2;
+
+assignedFreq = 2; %Count number of assigned frequencies (Use this with nFreq to check if all available frequencies are used)
+
+for freq = 3:length(fbest)
+    indexes = find(fbest);
+    %next_node = findSmallestDist(d, indexes)
+    %TODO: create a function that find the node with the shortest longest
+    %dist. to nodes that has a frequency assigned
+end
 end
