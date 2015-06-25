34f21714899b7145a7ab3f50e4f721dc43d0b79d
Marino <mabrgr@gmail.com>
2015-06-25 16:32:16 +0200

    Added functionality if two links are shortest distance to next link we want to assign with a new frequency

--- findNextFreq.m
+++ findNextFreq.m
@@ -5,10 +5,27 @@ frqs = 1:nFreq;
 relevantd = d(indexes, link);
 while length(find(frqs)) > 1
     [mindist, mindex] = min(relevantd);
+    relevantd(mindex) = []; %Removing shortest distances
     [row, ~] = find(d==mindist); %Finds link that are closest (and not checked)
     row(find(row==link)) = []; %Removing the value of the link we want to find a frequency for
+    
+    %If there are two links that are closest to the next link, then we
+    %need to find which frequency that are second closest to the next link
+    %and remove that frequency.
+    if (length(row) == 2)
+        mindex = find(min(relevantd));
+        relevantd(mindex) = [];
+        [mindist, ~] = min(relevantd);
+        mindex = find(d(:,link) == mindist);
+        freq = fbest(mindex);
+        if frqs(fbest(row(1))) == freq
+            row(2) = [];
+        else
+            row(1) = [];
+        end
+    end
+    
     frqs(fbest(row)) = 0; % Removing frequencies that are closest
-    relevantd(mindex) = []; %Removing shortest distances
 end
 frequency = find(frqs);
 end
